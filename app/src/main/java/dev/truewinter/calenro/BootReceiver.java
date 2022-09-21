75103b7484877812a18c2d844124c932ec570c17
TrueWinter <nicholis@truewinter.dev>
2022-09-21 18:34:28 +0200

    start alarmmanager after app is updated

--- app/src/main/java/dev/truewinter/calenro/BootReceiver.java
+++ app/src/main/java/dev/truewinter/calenro/BootReceiver.java
@@ -7,7 +7,8 @@ import android.content.Intent;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) ||
+            Intent.ACTION_MY_PACKAGE_REPLACED.equals(intent.getAction())) {
             ScheduledTaskManager.startAlarmManagerIfNotStarted(context);
         }
     }
